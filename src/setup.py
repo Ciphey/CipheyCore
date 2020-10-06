f9ce42973d141a6cfd506ec7a423443293d29201
Mr-Salme <72150568+Mr-Salme@users.noreply.github.com>
2020-10-06 09:47:08 -0500

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, Extension
 
-# Compile parts of `swig.hpp` into a shared library so we can call it from Python
+# Compile parts of `freq.cpp` into a shared library so we can call it from Python
 setup(
     #...
     ext_modules=[Extension('gof_test', ['freq.cpp'],),],
